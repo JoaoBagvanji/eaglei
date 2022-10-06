cc027ab5f6acd746627bf6f9fd20eb0df27798d3
JoaoBagvanji <joaobagvanji99@gmail.com>
2022-10-06 08:29:15 +0200

    update 06102022

--- src/screens/Viaturas.tsx
+++ src/screens/Viaturas.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { VStack, HStack, View, Text, Icon, useTheme,FlatList } from 'native-base';
-import { Car,Truck, Gear } from 'phosphor-react-native'
+import { Car, House, ListChecks, Gear, Users as Usuarios, Truck } from 'phosphor-react-native'
 import { Header } from '../components/Header';
 import { StyleSheet, TouchableOpacity } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -10,6 +10,7 @@ import  Inventario from '../screens/Inventario';
 import  Users from '../screens/Users';
 import  Tarefa  from '../screens/Tarefa';
 import  {useNavigation}  from '@react-navigation/native';
+import { Home } from './Home';
 
 
 const Stack = createStackNavigator();
@@ -22,6 +23,7 @@ export default function MyStack() {
             <Stack.Screen name="Tarefa" component={Tarefa} />
             <Stack.Screen name="Inventario" component={Inventario} />
             <Stack.Screen name="Users" component={Users} />
+            <Stack.Screen name="Home" component={Home} />
         </Stack.Navigator>
       </NavigationContainer>
     );
@@ -51,25 +53,39 @@ export function Viaturas() {
                 <Icon as ={<Car color={colors.green[500]}/>} />
             </HStack>
       </VStack>
-      <VStack flex={1} px={6}>
-        <View flex={4} backgroundColor={colors.gray[700]} flexDirection="row" justifyContent='space-evenly' alignItems='center'>
-            <TouchableOpacity style={styles.menuButtonsUp} onPress={() => navigation.navigate('Users')}>
-            <Icon as ={<Truck color={colors.primary[700]}/>} />
-                <Text >Viaturas</Text>
+      <VStack flex={4} px={6}>
+        <View flex={4} backgroundColor={colors.white} flexDirection="row" justifyContent='space-evenly' alignItems='center'  >
+            <TouchableOpacity activeOpacity={0.7} style={styles.menuButtonsUp} onPress={() => navigation.navigate('Users')}>
+            <Icon as ={<Usuarios color={colors.primary[700]}/>} />
+                <Text >Usuarios</Text>
             </TouchableOpacity>
 
-            <TouchableOpacity  style={styles.menuButtonsUp} onPress={() => navigation.navigate('Inventario')}>
+            <TouchableOpacity activeOpacity={0.7} style={styles.menuButtonsUp} onPress={() => navigation.navigate('Inventario')}>
             <Icon as ={<Gear color={colors.primary[700]}/>} />
             <Text >Inventario</Text>
             </TouchableOpacity>
-            </View>
+        </View>
+
+        <View flex={4} backgroundColor={colors.white} flexDirection="row" justifyContent='space-evenly' alignItems='center' paddingBottom={12}>
+            <TouchableOpacity activeOpacity={0.7} style={styles.menuButtonsUp} onPress={() => navigation.navigate('Tarefa')}>
+            <Icon as ={<ListChecks color={colors.primary[700]}/>} />
+                <Text >Tarefa</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity activeOpacity={0.7} style={styles.menuButtonsUp} onPress={() => navigation.navigate('Home')}>
+            <Icon as ={<House color={colors.primary[700]}/>} />
+            <Text >Home</Text>
+            </TouchableOpacity>
+        </View>
+
+
       </VStack>
     </VStack>
   );
 }
 const styles = StyleSheet.create({
     menuButtonsUp:{
-        backgroundColor: 'lightgrey',
+        backgroundColor: '#f5f5f5',
         width: '42%',
         height:'80%',
         alignItems: 'center',
