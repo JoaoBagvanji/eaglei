38cfb64f6dacdff56a35c4a969c78001cf73918c
JoaoBagvanji <joaobagvanji99@gmail.com>
2022-10-10 16:10:47 +0200

    update 16h10

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,5 +1,5 @@
-import { VStack, Heading, Icon, useTheme, View, HStack,Text } from 'native-base';
-import { User, Key } from 'phosphor-react-native'
+import { VStack, Icon, useTheme, View, HStack,Text } from 'native-base';
+import { SignIn, Key, User } from 'phosphor-react-native'
 import { useState } from 'react';
 import {  TextInput } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
@@ -7,7 +7,8 @@ import { StyleSheet } from 'react-native';
 
 import Logo from '../assets/eagle I 1.svg' ;
 import { Button } from '../components/Button';
-import { Input } from '../components/Input';
+import { Fontisto } from '@expo/vector-icons';
+
 
 
 export default function Login(){
@@ -22,6 +23,7 @@ export default function Login(){
     const { colors } = useTheme();
     const { fonts } = useTheme();
     const [isLoading, setIsLoading] = useState(false);
+    
 
     return(
         <VStack flex={1} alignItems="center" bg="white" px={8} pt={8} justifyContent='center'>
@@ -34,21 +36,37 @@ export default function Login(){
 
             <View style={styles.uinputView}>
                 <TextInput 
-                    style={styles.txtInput} selectionColor='#12375C' outlineColor='#cce3f9'
-                    activeOutlineColor='#12375C' underlineColor='#12375C' 
-                    left={<TextInput.Icon icon="account-outline"/>}
-                    mode="outlined" label="Username"
+                    style={styles.txtInput} 
+                    selectionColor='#12375C' 
+                    outlineColor='#cce3f9'
+                    activeOutlineColor='#12375C' 
+                    underlineColor='#12375C' 
+                    left={<TextInput.Icon icon={User}
+                    color={colors.primary[600]} />}
+                    mode="outlined"
+                    label="Username"
+                    theme={{fonts:{regular:{fontFamily:fonts.body}}, colors:{placeholder: colors.primary[600]}}}
+                    
                 />
             </View>
             <View style={styles.pinputView}>
                 <TextInput 
-                    style={styles.txtInput} selectionColor='#12375C' outlineColor='#cce3f9'
-                    activeOutlineColor='#12375C' left={<TextInput.Icon icon="key-outline" />}
-                    mode="outlined" secureTextEntry={true} label="Password"
+                    style={styles.txtInput} 
+                    selectionColor='#12375C' 
+                    outlineColor='#cce3f9'
+                    activeOutlineColor='#12375C' 
+                    left={<TextInput.Icon icon={Key} 
+                    color={colors.primary[600]}/>}
+                    mode="outlined" 
+                    secureTextEntry={true} 
+                    label="Password"
+                    theme={{fonts:{regular:{fontFamily:fonts.body}}, colors:{placeholder: colors.primary[600]}}}
+                    
                 />
             </View>
 
             <Button 
+                leftIcon={<Icon as={<SignIn color={colors.green[700]}/>} ml={4}/>}
                 title='Entrar'
                 w="full"
                 onPress={handleLogin}
@@ -64,7 +82,7 @@ const styles= StyleSheet.create({
         height:56,
         width: "100%",
         justifyContent: 'center',
-        borderRadius: 6
+        borderRadius: 6,
     },
 
     pinputView:{
@@ -76,8 +94,12 @@ const styles= StyleSheet.create({
     },
 
     txtInput:{
-        backgroundColor: '#cce3f9',
+        backgroundColor: '#e0edf9',
         textColor: '#12375C',
-        
+       
     }, 
+    buttonIcon:{
+        fontSize: 16,
+        color: '#12375c'
+    },
 })
