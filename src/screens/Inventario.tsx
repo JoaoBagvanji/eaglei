2fa77319eca67575f5627b6dfbf425ea9e067a7d
JoaoBagvanji <joaobagvanji99@gmail.com>
2022-10-24 14:55:23 +0200

    update

--- src/screens/Inventario.tsx
+++ src/screens/Inventario.tsx
@@ -2,7 +2,7 @@ import React , {useState}from 'react';
 import { VStack, HStack, View, Text, Icon, useTheme} from 'native-base';
 import { Wrench, Notification, Checks, Handshake , Nut , UsersThree,ArrowsCounterClockwise , KeyReturn, ThumbsUp} from 'phosphor-react-native'
 import { Header } from '../components/Header';
-import { StyleSheet, SafeAreaView, FlatList, ActivityIndicator } from 'react-native';
+import { StyleSheet, SafeAreaView, FlatList, ActivityIndicator,TouchableOpacity } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import  {useNavigation}  from '@react-navigation/native';
@@ -19,6 +19,7 @@ import StockReturn from './pages_inventario/StockReturn';
 import Utilizadores from './pages_inventario/Utilizadores';
 import Inventarios from './Inventarios';
 
+
 const Stack = createStackNavigator();
 const ImagemEstac= 'https://images.unsplash.com/photo-1662581871665-f299ba8ace07?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80'
 const ImagemEstac_Ap= 'https://images.unsplash.com/photo-1603899122361-e99b4f6fecf5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
@@ -30,6 +31,7 @@ const ImagemEstac_St= 'https://images.unsplash.com/photo-1526152505827-d2f3b5b4a
 const ImagemEstac_Dp= 'https://images.unsplash.com/photo-1566576721346-d4a3b4eaeb55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80'
 
 const inventarios = [
+    
     {brand: 'Comserv', type: 'Pedido', name: 'Pedido', qtd: 22000, inventarioImage :  ImagemEstac, icon: <Notification color='#A1C861'/>},
     {brand: 'Comserv', type: 'Aprovados', name: 'Aprovado', qtd: 10, inventarioImage :  ImagemEstac_Ap, icon: <Checks color='#A1C861'/>},
     {brand: 'Comserv', type: 'Finalizados', name: 'Finalizado', qtd: 20, inventarioImage :  ImagemEstac_Fn, icon: <Handshake   color='#A1C861'/>},
@@ -38,14 +40,6 @@ const inventarios = [
     {brand: 'Comserv', type: 'Atenção', name: 'Attention', qtd: 660, inventarioImage :  ImagemEstac_At, icon: <ArrowsCounterClockwise   color='#A1C861'/>},
     {brand: 'Comserv', type: 'Retorno', name: 'StockReturn', qtd: 90, inventarioImage :  ImagemEstac_St, icon: <KeyReturn color='#A1C861'/>},
     {brand: 'Comserv', type: 'Despacho', name: 'Despacho', qtd: 80, inventarioImage :  ImagemEstac_Dp, icon: <ThumbsUp  color='#A1C861'/>}
-    {brand: 'Comserv', type: 'Pedido', name: 'Pedidos', qtd: 220, inventarioImage :  ImagemEstac},
-    {brand: 'Comserv', type: 'Aprovados', name: 'Aprovados', qtd: 10, inventarioImage :  ImagemEstac},
-    {brand: 'Comserv', type: 'Finalizados', name: 'Finalizados  ', qtd: 20, inventarioImage :  ImagemEstac},
-    {brand: 'Comserv', type: 'Armazem', name: 'Armazem  ', qtd:340, inventarioImage :  ImagemEstac},
-    {brand: 'Comserv', type: 'Users', name: 'Users', qtd: 230, inventarioImage :  ImagemEstac},
-    {brand: 'Comserv', type: 'Atencao', name: 'Atenção', qtd: 660, inventarioImage :  ImagemEstac},
-    {brand: 'Comserv', type: 'Relatórios', name: 'Relatórios', qtd: 90, inventarioImage :  ImagemEstac},
-    {brand: 'Comserv', type: 'Projectos', name: 'Projectos', qtd: 80, inventarioImage :  ImagemEstac}
 ]
 
 
@@ -72,14 +66,19 @@ export default function MyStack() {
 export  function Inventario() {
     type Nav ={
         navigate : (value: string) => void;
-      }
+    }
       
-      const { navigate } = useNavigation<Nav>()
-      
-    
-        const { fonts } = useTheme();
-        const { colors } = useTheme();
-        const [loadingMore, setLoadingMore] = useState(false);
+    const { navigate } = useNavigation<Nav>()
+    const { fonts } = useTheme();
+    const { colors } = useTheme();
+    const [loadingMore, setLoadingMore] = useState(false);
+
+    function handleTelas(posicao){
+        // Verficar a posicao e nome
+        // let telaposicao = inventarios.findIndex(x=>x.name==posicao.name);
+        // console.log(telaposicao,posicao)
+        navigate(posicao.name) as never
+    }
   return (
 <VStack flex={1} pb={6} bg="white">
 
@@ -101,6 +100,7 @@ export  function Inventario() {
         </HStack>
    
     </VStack>
+
         <VStack flex={4} mx={2} py={20}>
                 <SafeAreaView>
                     
@@ -120,23 +120,6 @@ export  function Inventario() {
                             }
                         />
                 </SafeAreaView>
-        <VStack flex={5}>
-            <SafeAreaView>
-                <FlatList
-                    numColumns={2} 
-                    data={inventarios} 
-                    keyExtractor={(item, index) => index.toString()}
-                    renderItem={ ( {item} ) => (<RectButton style={styles.container}><Inventarios inventario={item}/></RectButton>)}
-                    showsVerticalScrollIndicator ={false}
-                    onEndReachedThreshold={0.1}
-                    ListFooterComponent ={
-                        loadingMore 
-                        ? <ActivityIndicator color={colors.green[700]} />
-                        : <></>
-    
-                    }
-                />
-            </SafeAreaView>
         </VStack>
 
 </VStack>  );
@@ -148,7 +131,7 @@ const styles = StyleSheet.create({
         maxWidth: '45%',
         backgroundColor: colors.shape,
         borderRadius: 20,
-        paddingVertical: '5%',
+        paddingVertical: 15,
         alignItems: 'center',
         margin: '2%'
     },
