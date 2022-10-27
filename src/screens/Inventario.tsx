71b7bf2d6f923febd35f6ff92e862b115d1fc544
hmahesse <engmahesse@gmail.com>
2022-10-27 10:25:29 +0200

    27.10.2022

--- src/screens/Inventario.tsx
+++ src/screens/Inventario.tsx
@@ -25,16 +25,6 @@ const Stack = createStackNavigator();
 
 const inventarios = [
     
-
-    {label: 'Pedido', component_name: 'Pedido', qtd: 22000, icon: <Notification color='#A1C861'/>},
-    {label: 'Aprovados', component_name: 'Aprovado', qtd: 10, icon: <Checks color='#A1C861'/>},
-    {label: 'Finalizados', component_name: 'Finalizado', qtd: 20, icon: <Handshake   color='#A1C861'/>},
-    {label: 'Armazem', component_name: 'Armazem', qtd:340, icon: <Nut  color='#A1C861'/>},
-    {label: 'Utilizadores', component_name: 'Utilizadores', qtd: 230, icon: <UsersThree  color='#A1C861'/>},
-    {label: 'Atenção', component_name: 'Attention', qtd: 660,  icon: <ArrowsCounterClockwise   color='#A1C861'/>},
-    {label: 'Retorno', component_name: 'StockReturn', qtd: 90, icon: <KeyReturn color='#A1C861'/>},
-    {label: 'Despacho', component_name: 'Despacho', qtd: 80, icon: <ThumbsUp  color='#A1C861'/>},
-    
     {brand: 'Comserv', type: 'Pedido', name: 'Pedido', qtd: 22000, icon: <Notification color='#A1C861'/>},
     {brand: 'Comserv', type: 'Aprovados', name: 'Aprovado', qtd: 10, icon: <Checks color='#A1C861'/>},
     {brand: 'Comserv', type: 'Finalizados', name: 'Finalizado', qtd: 20, icon: <Handshake   color='#A1C861'/>},
