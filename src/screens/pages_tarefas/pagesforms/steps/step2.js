62ddc26f70bfac2e0e582e5da490713ad645d821
JoaoBagvanji <jbagvanji54@gmail.com>
2022-11-08 16:19:11 +0200

    alteracoes: steps: com questoes

--- src/screens/pages_tarefas/pagesforms/steps/step2.js
+++ src/screens/pages_tarefas/pagesforms/steps/step2.js
@@ -2,7 +2,10 @@ import React, { Component } from "react";
 import { Image, View, TouchableOpacity, TextInput, Text } from "react-native";
 
 import styles from "./styles";
+import {  Icon, Radio, Stack } from 'native-base';
 
+import {  CaretCircleRight, CaretCircleLeft  } from 'phosphor-react-native'
+import colors from "../../../../styles/colors";
 export class step2 extends Component {
   constructor(props) {
     super(props);
@@ -33,36 +36,59 @@ export class step2 extends Component {
         <View>
           <Text
             style={styles.currentStepText}
-          >{`Step ${currentStep} of ${totalSteps}`}</Text>
+          >{`Questão ${currentStep} de ${totalSteps}`}
+          </Text>
         </View>
-        <TextInput
+        <View>
+            <Text
+                style={styles.currentText}
+              > Para a actividade a ser desenvolvida {'\n'}
+                está disponível um kit de derrames {'\n'}
+                ambientais
+              </Text>
+        </View>
+          
+          <Radio.Group
+             defaultValue="1" name="exampleGroup" accessibilityLabel="favorite colorscheme" mb={5}
+          > 
+            <Stack direction={{
+                  base: "row",
+                  md: "row"
+                }} alignItems={{
+                  base: "flex-start",
+                  md: "center"
+                }} space={10} w="75%" maxW="300px">
+                <Radio colorScheme="emerald" value="1" my={1} size='sm'>
+                  sim
+                </Radio>
+                <Radio colorScheme="secondary" value="2" my={1} size='sm'>
+                  não
+                </Radio>
+                <Radio colorScheme="warning" value="3" my={1} size='sm'>
+                  n\a
+                </Radio>
+            </Stack>
+          </Radio.Group>
+        {/* <TextInput
           style={styles.input}
           onChangeText={text => this.setState({ text })}
           value={this.state.text}
           placeholder={"Email"}
-          placeholderTextColor="#fff"
+          placeholderTextColor={colors.gray}
         />
         <TextInput
           style={styles.input}
           onChangeText={text => this.setState({ text })}
           value={this.state.text}
           placeholder={"Phone Number"}
-          placeholderTextColor="#fff"
-        />
+          placeholderTextColor={colors.gray}
+        /> */}
         <View style={[styles.btnContainer, styles.marginAround]}>
           <TouchableOpacity onPress={this.props.back} style={styles.btnStyle}>
-            <Image
-              source={require("../../../../assets/arrow.png")}
-              style={[styles.btnImage, styles.backBtn]}
-              resizeMode="cover"
-            />
+            <Icon as ={<CaretCircleLeft  color={colors.blue} size={60}/>} />
           </TouchableOpacity>
           <TouchableOpacity onPress={this.nextStep} style={styles.btnStyle}>
-            <Image
-              source={require("../../../../assets/arrow.png")}
-              style={styles.btnImage}
-              resizeMode="cover"
-            />
+            <Icon as ={<CaretCircleRight color={colors.blue} size={60}/>} />
           </TouchableOpacity>
         </View>
       </View>
