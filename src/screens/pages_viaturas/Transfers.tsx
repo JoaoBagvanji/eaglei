5190fff7820d65d039caa0f0304016ccd33b8142
Claudio Bucene <claudiobucene@comserv.local>
2022-11-22 08:37:40 +0200

    Starting to implement formTransfers

--- src/screens/pages_viaturas/Transfers.tsx
+++ src/screens/pages_viaturas/Transfers.tsx
@@ -1,36 +1,77 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { VStack, HStack, View, Text, Icon, useTheme } from 'native-base';
-import { ArrowLeft, Truck ,ArrowRight } from 'phosphor-react-native'
+import { MagnifyingGlass, Plus } from 'phosphor-react-native'
 import { Header } from '../../components/Header';
+import colors from '../../styles/colors';
 
-export default function Transfers() {
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import  {useNavigation}  from '@react-navigation/native';
+
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import FormTransferencia from '../../routes/trans.routes';
+
+const Stack = createStackNavigator();
+
+export default function MyStack() {
+    return (
+     <NavigationContainer 
+     independent={true}
+     >
+       <Stack.Navigator  screenOptions={{headerShown: false}} 
+               initialRouteName='Transferencia'>
+           <Stack.Screen name="Transferencia" component={Transferencia} />
+           <Stack.Screen name="FormTransferencia" component={FormTransferencia} />
+       </Stack.Navigator>
+     </NavigationContainer>
+   );
+ }
+
+export function Transferencia({navigation}) {
+
+    type Nav ={
+        navigate : (value: string) => void;
+    }
+      
+    const { navigate } = useNavigation<Nav>()
 
     const { fonts } = useTheme();
     const { colors } = useTheme();
-
-
-
+    
+    
   return (
     <VStack flex={1} pb={6} bg="white">
         
         <VStack flex={1} px={6}>
             <HStack w="full" mt={8} mb={4} justifyContent="space-between" alignItems='center' flexDirection="row">
                 <View>
-                    <Text color="primary.800" fontSize="md" fontFamily={fonts.heading}>
-                        Navegue 
-                    </Text>
-                    <Text color="primary.800" fontSize="md" fontFamily={fonts.body}>
-                        entre as Transferências
-                    </Text>
+                <Text color="primary.800" fontSize="md" fontFamily={fonts.heading}>
+                    Navegue 
+                </Text>
+                <Text color="primary.800" fontSize="md" fontFamily={fonts.body}>
+                    entre as Transferências
+                </Text>
                 </View>
-                <View>
-                    <Icon as ={<ArrowLeft color={colors.green[700]} size={16}/>} />
-                    <Icon as ={<Truck color={colors.primary[600]}/>}/>
-                    <Icon as ={<ArrowRight color={colors.green[700]} size={16}/>} />
-                </View>
-                
+                <Icon as ={<MagnifyingGlass color={colors.green[700]}/>} />
             </HStack>
       </VStack>
+    
+       <VStack mb={'20%'} mr={'5%'} alignSelf={'flex-end'}>
+            <TouchableOpacity style={styles.formButton}  onPress={() => navigate('FormTransferencia') as never}>
+                <Plus size={22} color={colors.green[700]} />
+            </TouchableOpacity>
+       </VStack>
+
     </VStack>
   );
 }
+
+const styles = StyleSheet.create({
+    formButton:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: colors.blue,
+        borderRadius: 25,
+        height: 50,
+        width:50
+}})
